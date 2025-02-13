1aa7eaa299a482c732b925cb1050ca9000d238df
SCAI3 <gabriele.scagliotti.it@gmail.com>
2025-02-13 16:11:56 +0100

    bb

--- corsopnrr/Assets/Kunai/kunaispawner.cs
+++ corsopnrr/Assets/Kunai/kunaispawner.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class kunaispawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //l'asset del proiettile
+    [SerializeField]
+    private GameObject proiettile;
+
+    //punto da dove inizio a sparare
+    [SerializeField]
+    private Transform firepoint;
+      
+    //azione che utente fa per sparare
+    [SerializeField]
+    private InputActionReference ShootAction;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ShootAction.action.WasPresseedThisFrame() =>
+       //se ho premuto in questo frame da come valore vero altrimenti da come valore falso
+
+
+     if(ShootAction.action.WasPressedThisFrame()){
+       //instantiate => crea il proiettile nella posizione firePoint.position,con rotazione firePoint.rotation
+        Instantiate(proiettile, firepoint.position,firepoint.rotation);
+     }    
     }
 }
